e501ae8ae99267e06c631017f4af0d2530589420
Alexander Högberg <alexhogberg@gmail.com>
2017-02-04 16:43:47 +0100

    Minor fixes

--- afterwork.py
+++ afterwork.py
@@ -1,50 +1,47 @@
 import boto3
-from boto3.dynamodb.conditions import Attr, Key
+from boto3.dynamodb.conditions import Attr
 from datetime import datetime, timedelta
 import calendar
 from botocore import exceptions
 
-def default(x, e, y):
-    try:
-        return x()
-    except e:
-        return y
 
-class Afterwork():
+class Afterwork:
     def __init__(self):
         self.dynamodb = boto3.resource('dynamodb')
         self.awtable = self.dynamodb.Table('afterworks')
 
         self.valid_commands = ['list', 'create', 'join', 'leave', 'delete']
 
-        self.valid_days = ['mo', 'tue', 'wed', 'thur', 'fri',
-                      'monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+        self.valid_days = [
+            'mo', 'tue', 'wed', 'thur', 'fri',
+            'monday', 'tuesday', 'wednesday', 'thursday', 'friday'
+        ]
 
-    def parse_command(self, command, event):
-        if command == "":
-            return self.private_slack_text("No command given, Possible commands are: list, create <day> <time> <place>, join <day>, leave <day>, delete <day>")
-        elif command.split(" ")[0] in self.valid_commands:
-            operation = command.split(" ")
-
-            action = getattr(self, operation[0] + "_afterwork")
-            return action(operation, event)
-
-        else:
-            return self.private_slack_text(
-                "Incorrect command specified. Possible commands are: list, create <day> <time> <place>, join <day>, leave <day>")
-
-    def private_slack_text(self, text):
+    @staticmethod
+    def __private_slack_text(text):
         return {
-            "text" : text
+            "text": text
         }
 
-    def public_slack_text(self, text, channel):
+    @staticmethod
+    def __public_slack_text(text, channel):
         return {
             "response_type": "in_channel",
             "text": "<#" + channel + ">: " + text
         }
 
-    def is_day_valid(self, day_string):
+    @staticmethod
+    def __get_next_weekday(startdate, weekday):
+        start = datetime.strptime(startdate, '%Y-%m-%d')
+        day = timedelta((7 + weekday - start.weekday()) % 7)
+
+        return (start + day).strftime("%Y-%m-%d")
+
+    @staticmethod
+    def __get_user_name(event):
+        return "<@" + event['user_id'] + "|" + event['user_name'] + ">"
+
+    def __is_day_valid(self, day_string):
         print(day_string)
         if day_string in self.valid_days:
             if day_string in ['mo', 'monday']:
@@ -57,15 +54,27 @@ class Afterwork():
                 day_num = 3
             elif day_string in ['fri', 'friday']:
                 day_num = 4
+            else:
+                day_num = None
             return day_num
         else:
             return None
 
-    def get_next_weekday(self, startdate, weekday):
-        start = datetime.strptime(startdate, '%Y-%m-%d')
-        day = timedelta((7 + weekday - start.weekday()) % 7)
+    def parse_command(self, command, event):
+        if command == "":
+            return self.__private_slack_text(
+                """No command given, Possible commands are: \nlist \ncreate <day> <time> <place>\njoin <day>\nleave <day>\ndelete <day>""")
+        elif command.split(" ")[0] in self.valid_commands:
+            operation = command.split(" ")
 
-        return (start + day).strftime("%Y-%m-%d")
+            action = getattr(self, operation[0] + "_afterwork")
+            return action(operation, event)
+
+        else:
+            return self.__private_slack_text(
+                """Incorrect command specified. Possible commands are: \nlist \ncreate <day> <time> <place>\njoin <day>\nleave <day>\ndelete <day>""")
+
+
 
     def list_afterwork(self, command, event):
         results = self.awtable.scan(
@@ -92,9 +101,9 @@ class Afterwork():
                     for participant in item['Participants']:
                         events += participant
                 events += "\n"
-            return self.private_slack_text(events)
+            return self.__private_slack_text(events)
         else:
-            return self.private_slack_text("No upcoming after work.. Perhaps you want to create one?")
+            return self.__private_slack_text("No upcoming after work.. Perhaps you want to create one?")
 
     def create_afterwork(self, command, event):
 
@@ -105,7 +114,7 @@ class Afterwork():
             time = "17:30"
 
         if len(command) > 3:
-            place = command[3:]
+            place = " ".join(command[3:])
         else:
             place = 'Unspecified location'
 
@@ -113,9 +122,9 @@ class Afterwork():
 
         author = self.__get_user_name(event)
 
-        day_num = self.is_day_valid(day)
+        day_num = self.__is_day_valid(day)
         if day_num is not None:
-            date = self.get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
+            date = self.__get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
             try:
                 self.awtable.put_item(
                     Item={
@@ -123,27 +132,30 @@ class Afterwork():
                         'Location': place,
                         'Time': time,
                         'Author': author,
-                        'Participants' : [author]
+                        'Participants': [author]
                     },
                     ConditionExpression="attribute_not_exists(#d)",
                     ExpressionAttributeNames={
                         '#d': 'Date'
                     }
                 )
-            except exceptions.ClientError as e:
-                return self.private_slack_text("Couldn't create after work. It seems as if there is already an after work planned that day.")
+            except exceptions.ClientError:
+                return self.__private_slack_text(
+                    "Couldn't create after work. It seems as if there is already an after work planned that day.")
 
-            return self.public_slack_text("Hi! %s created an after work on %s at %s by %s! To join type /afterwork join %s" % (author, date, time, place, day), channel)
+            return self.__public_slack_text(
+                """Hi! %s created an after work on %s at %s by %s! \n To join type /afterwork join %s""" % (author, date, time, place, day), channel)
         else:
-            return self.private_slack_text("You cannot create an afterwork on that day! Valid days are monday to friday")
+            return self.__private_slack_text(
+                "You cannot create an afterwork on that day! Valid days are monday to friday")
 
     def join_afterwork(self, command, event):
         day = command[1]
         author = self.__get_user_name(event)
 
-        day_num = self.is_day_valid(day)
+        day_num = self.__is_day_valid(day)
         if day_num is not None:
-            date = self.get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
+            date = self.__get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
             try:
                 self.awtable.update_item(
                     Key={
@@ -158,21 +170,20 @@ class Afterwork():
                     ReturnValues="UPDATED_NEW"
                 )
 
-                return self.private_slack_text("Great! You've joined the after work on " + day + "!")
-            except Exception as e:
-                return self.private_slack_text("I'm sorry, I couldn't join you to that after work. Perhaps you are already participating?")
+                return self.__private_slack_text("Great! You've joined the after work on " + day + "!")
+            except exceptions.ClientError:
+                return self.__private_slack_text(
+                    "I'm sorry, I couldn't join you to that after work. Perhaps you are already participating?")
         else:
-            return self.private_slack_text("Sorry, I couldn't find an after work that day.")
-
-
+            return self.__private_slack_text("Sorry, I couldn't find an after work that day.")
 
     def leave_afterwork(self, command, event):
         day = command[1]
         author = self.__get_user_name(event)
 
-        day_num = self.is_day_valid(day)
+        day_num = self.__is_day_valid(day)
         if day_num is not None:
-            date = self.get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
+            date = self.__get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
 
             result = self.awtable.get_item(
                 Key={
@@ -196,11 +207,11 @@ class Afterwork():
                         },
                         ReturnValues="UPDATED_NEW"
                     )
-                    return self.private_slack_text("You are now removed from the after work!")
-                except Exception:
-                    return self.private_slack_text("Oops. Something went wrong when removing you from the after work!")
+                    return self.__private_slack_text("You are now removed from the after work!")
+                except exceptions.ClientError:
+                    return self.__private_slack_text("Oops. Something went wrong when removing you from the after work!")
             else:
-                return self.private_slack_text("Are you really joined to that after work?")
+                return self.__private_slack_text("Are you really joined to that after work?")
         return "Couldn't find any after work on that day."
 
     def delete_afterwork(self, command, event):
@@ -208,9 +219,9 @@ class Afterwork():
         author = self.__get_user_name(event)
         channel = event['channel_id']
 
-        day_num = self.is_day_valid(day)
+        day_num = self.__is_day_valid(day)
         if day_num is not None:
-            date = self.get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
+            date = self.__get_next_weekday(datetime.now().strftime("%Y-%m-%d"), day_num)
             try:
                 self.awtable.delete_item(
                     Key={
@@ -218,14 +229,11 @@ class Afterwork():
                     },
                     ConditionExpression="Author = :i",
                     ExpressionAttributeValues={
-                        ':i' : author
+                        ':i': author
                     }
                 )
-                return self.public_slack_text("The after work on %s has now been cancelled, sorry!" % day, channel)
+                return self.__public_slack_text("The after work on %s has now been cancelled, sorry!" % day, channel)
             except Exception as e:
-                return self.private_slack_text(e)
+                return self.__private_slack_text(e)
         else:
-            return self.private_slack_text("That after work doesn't seem to exist")
-
-    def __get_user_name(self, event):
-        return "<@" + event['user_id'] + "|" + event['user_name'] + ">"
+            return self.__private_slack_text("That after work doesn't seem to exist")
