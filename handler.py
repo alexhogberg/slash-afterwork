c5790638c09b8c14aff542fad6fa5ab8ab3eafb3
Alexander Högberg <alexhogberg@gmail.com>
2017-02-13 14:21:09 +0100

    Fixed typo in handler

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ afterwork_handler = Afterwork()
 
 
 def index(event, context):
-    if 'body' in event and 'token' in event['body'] and event['body']['token'] == os.environ['apiKey']:
+    if 'body' in event and 'token' in event['body'] and event['body']['token'] == os.environ['authKey']:
         return afterwork_handler.parse_command(event['body']['text'], event['body'])
     elif 'triggerToken' in event and event['triggerToken'] == os.environ['authKey']:
         return afterwork_handler.todays_afterwork()
